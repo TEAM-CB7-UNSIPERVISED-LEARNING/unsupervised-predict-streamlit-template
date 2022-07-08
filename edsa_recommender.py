6b4fe1661b94a0d41b9ed84b012c4127db07cabd
katlego maponya <mr015khai@gmail.com>
2022-07-08 03:45:43 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -46,7 +46,7 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Recommender System","Solution Overview", "Team"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
