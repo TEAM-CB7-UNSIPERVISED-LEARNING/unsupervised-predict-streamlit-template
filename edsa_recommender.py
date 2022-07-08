fde5277beade653334e0af8ac5a14d1968751a10
katlego maponya <mr015khai@gmail.com>
2022-07-08 12:44:35 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -105,7 +105,7 @@ def main():
         st.title("Solution Overview")
         st.write("Describe your winning approach on this page")
         st.write("we build this app look for movies using filtering and contend basesd algorythms")
-        st.image("https://www.naukri.com/learning/articles/movie-recommendation-system-using-machine-learning/")
+        st.image("https://www.researchgate.net/profile/Lionel-Ngoupeyou-Tondji/publication/323726564/figure/fig5/AS:631605009846299@1527597777415/Content-based-filtering-vs-Collaborative-filtering-Source.png")
 
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
