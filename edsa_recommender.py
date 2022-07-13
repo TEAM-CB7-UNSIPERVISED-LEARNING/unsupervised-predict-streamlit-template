8ce7dd17cbc37ddfc046a29075b1b8b0fde6439a
katlego maponya <mr015khai@gmail.com>
2022-07-13 20:02:31 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -339,7 +339,7 @@ def main():
         
     #dataset page
     if page_selection == "Datasets":
-        st.write("Datasets")
+        st.info("Datasets")
         st.image("https://intellipaat.com/mediaFiles/2018/07/Structured-Data-Vs.-Unstructured-Data.png")
         st.write("Raw Data Used that was used to test and train our models")
         if st.checkbox("check raw data"):
