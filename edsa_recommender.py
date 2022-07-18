7ed5c3bc418b8e7fa65b753fe31dc1e38f46f931
katlego maponya <mr015khai@gmail.com>
2022-07-18 21:55:11 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -202,7 +202,7 @@ def main():
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
         st.write("solution")
-        st.write("we build this app to look for movies using collaborative filtering and contend basesd filtering recommendation sytems")
+        st.info("we build this app to look for movies using collaborative filtering and contend basesd filtering recommendation sytems")
         st.image("https://www.researchgate.net/profile/Lionel-Ngoupeyou-Tondji/publication/323726564/figure/fig5/AS:631605009846299@1527597777415/Content-based-filtering-vs-Collaborative-filtering-Source.png")
         if st.checkbox("overview"):
             st.write("### CONTENT BASED FILTERING")
