2adc968e40bf76d87542f450fb1b27edd8483fa1
katlego maponya <mr015khai@gmail.com>
2022-07-27 23:24:32 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -158,7 +158,28 @@ def main():
             st.write(df)
         if len(df) ==0:
             st.write('We have no movies for that rating!')        
-       
+        def youtube_link(title):
+        
+            """This function takes in the title of a movie and returns a Search query link to youtube
+    
+            INPUT: ('Avengers age of ultron')
+            -----------
+    
+            OUTPUT: https://www.youtube.com/results?search_query=The+little+Mermaid&page=1
+            ----------
+            """
+            title = title.replace(' ','+')
+            base = "https://www.youtube.com/results?search_query="
+            q = title
+            page = "&page=1"
+            URL = base + q + page
+            return URL            
+        if len(df) !=0:           
+            for _, row in df.iterrows():
+                st.write(row['title'])
+                st.write(youtube_link(title = row['title']))
+                
+            st.image("https://miro.medium.com/max/1400/0*ckAOzr7BW6fhFeGK.jpg")
                 
 
 
