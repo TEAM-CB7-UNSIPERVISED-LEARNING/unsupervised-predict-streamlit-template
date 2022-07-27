cce44eabdb847409d4a977c84121260580197888
Sidney Abrahams <sidneyabrahamsrsa@gmail.com>
2022-07-27 16:36:07 +0200

    changed movie list ranges

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -82,9 +82,9 @@ def main():
 
         # User-based preferences
         st.write('### Enter Your Three Favorite Movies')
-        movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
-        movie_2 = st.selectbox('Second Option',title_list[25055:25255])
-        movie_3 = st.selectbox('Third Option',title_list[21100:21200])
+        movie_1 = st.selectbox('Fisrt Option',title_list[0:800])
+        movie_2 = st.selectbox('Second Option',title_list[800:1600])
+        movie_3 = st.selectbox('Third Option',title_list[1600:2400])
         fav_movies = [movie_1,movie_2,movie_3]
 
         # Perform top-10 movie recommendation generation
