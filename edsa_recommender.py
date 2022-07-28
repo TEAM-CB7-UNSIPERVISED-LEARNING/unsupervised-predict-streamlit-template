7d70ec5072319bc40c87dc58e6954e4a62d7d7ca
katlego maponya <mr015khai@gmail.com>
2022-07-28 13:06:23 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -38,7 +38,7 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Welcome","About","Search for a movie","Recommender System","Solution Overview","Team Profile","Data analysis","Information","Glossary","Reviews","History",]
+    page_options = ["Welcome","About","Search for a movie","Recommender System","Solution Overview","Team Profile","Data analysis","Information","Reviews","History",]
     
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
